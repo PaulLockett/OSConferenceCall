b772390aeba78658dd4c4d2191f63037b46cec0a
PaulLockett <plockett.pl@gmail.com>
2021-12-09 00:28:18 -0500

    Add remove client code from server executable

--- server.py
+++ server.py
@@ -15,17 +15,6 @@ def start_listening():
     t1.start()
     t2.start()
 
-def start_camera_stream():
-    camera_client = CameraClient(text_target_ip.get(1.0, 'end-1c'), 7777)
-    t3 = threading.Thread(target=camera_client.start_stream)
-    t3.start()
-
-def start_audio_stream():
-    audio_sender = AudioSender(text_target_ip.get(1.0, 'end-1c'), 6666)
-    t4 = threading.Thread(target=audio_sender.start_stream)
-    t4.start()
-
-
 window = tk.Tk()
 window.title("Server")
 window.geometry("300x200")
@@ -38,12 +27,6 @@ text_target_ip.pack()
 
 btn_listen = tk.Button(window, text="Start Listening", width=50, command=start_listening)
 btn_listen.pack(anchor=tk.CENTER, expand=True)
-
-btn_camera = tk.Button(window, text="Start Camera", width=50, command=start_camera_stream)
-btn_camera.pack(anchor=tk.CENTER, expand=True)
-
-btn_audio = tk.Button(window, text="Start Audio", width=50, command=start_audio_stream)
-btn_audio.pack(anchor=tk.CENTER, expand=True)
 
 btn_chat = tk.Button(window, text="Start Chat", width=50)
 btn_chat.pack(anchor=tk.CENTER, expand=True)
