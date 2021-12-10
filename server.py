6f88f2dfe38297a2a89362315316a09822ce444c
PaulLockett <plockett.pl@gmail.com>
2021-12-10 04:26:38 -0500

    add chat and stop functionality

--- server.py
+++ server.py
@@ -7,20 +7,26 @@ import threading
 local_ip_address = socket.gethostbyname(socket.gethostname())
 
 server = StreamingServer(local_ip_address, 9999)
-receiver = AudioReceiver(local_ip_address, 8888)
+receiver = AudioServer(local_ip_address, 8888)
+chatRoom = ChatServer(local_ip_address, 7777)
 
 def start_server():
     t1 = threading.Thread(target=server.start_server)
     t2 = threading.Thread(target=receiver.start_server)
+    t3 = threading.Thread(target=chatRoom.start)
     t1.start()
     t2.start()
+    t3.start()
+
+def stop_server():
+    server.stop_server()
+    receiver.stop_server()
+    chatRoom.stop()
+    exit()
 
 window = tk.Tk()
 window.title("Server")
 window.geometry("300x200")
-
-label_tartget_ip = tk.Label(window, text="Target IP: ")
-label_tartget_ip.pack()
 
 text_target_ip = tk.Text(window, height=1)
 text_target_ip.pack()
@@ -28,7 +34,7 @@ text_target_ip.pack()
 btn_listen = tk.Button(window, text="Start Server", width=50, command=start_server)
 btn_listen.pack(anchor=tk.CENTER, expand=True)
 
-btn_chat = tk.Button(window, text="Start Chat", width=50)
-btn_chat.pack(anchor=tk.CENTER, expand=True)
+btn_stop = tk.Button(window, text="Stop Server", width=50, command=stop_server)
+btn_stop.pack(anchor=tk.CENTER, expand=True)
 
 window.mainloop()
