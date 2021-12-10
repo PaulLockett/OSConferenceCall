4a9fcd668ceb209f163e0ff4345cde28eedbe4d2
PaulLockett <plockett.pl@gmail.com>
2021-12-09 23:56:11 -0500

    change function and UI names

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ local_ip_address = socket.gethostbyname(socket.gethostname())
 server = StreamingServer(local_ip_address, 9999)
 receiver = AudioReceiver(local_ip_address, 8888)
 
-def start_listening():
+def start_server():
     t1 = threading.Thread(target=server.start_server)
     t2 = threading.Thread(target=receiver.start_server)
     t1.start()
@@ -25,7 +25,7 @@ label_tartget_ip.pack()
 text_target_ip = tk.Text(window, height=1)
 text_target_ip.pack()
 
-btn_listen = tk.Button(window, text="Start Listening", width=50, command=start_listening)
+btn_listen = tk.Button(window, text="Start Server", width=50, command=start_server)
 btn_listen.pack(anchor=tk.CENTER, expand=True)
 
 btn_chat = tk.Button(window, text="Start Chat", width=50)
